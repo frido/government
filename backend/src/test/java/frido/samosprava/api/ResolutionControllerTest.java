64c85e1eb7ee340864486664830df71c0b7faf34
Peter <peter.fridrich@outlook.sk>
2019-07-26 07:49:40 +0200

    tests

--- backend/src/test/java/frido/samosprava/api/ResolutionControllerTest.java
+++ backend/src/test/java/frido/samosprava/api/ResolutionControllerTest.java
@@ -2,6 +2,7 @@ package frido.samosprava.api;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import frido.samosprava.core.client.JdkHttpClient;
@@ -11,14 +12,26 @@ import frido.samosprava.core.store.DataStore;
 import frido.samosprava.core.store.HttpStore;
 
 public class ResolutionControllerTest {
+	
+	private static InMemoryCollections collections;
+
+	@BeforeAll
+	public static void init(){
+		WebClient client = new JdkHttpClient();
+		DataStore store = new HttpStore(client);
+		collections = new InMemoryCollections(store, "https://frido.github.io/government/db/");
+	}
 
 	@Test
 	public void testResolution() throws Exception {
-		WebClient client = new JdkHttpClient();
-		DataStore store = new HttpStore(client);
-		InMemoryCollections collections = new InMemoryCollections(store, "https://frido.github.io/government/db/");
 		ResolutionController controller = new ResolutionController(collections);
 		assertEquals(1001, controller.resolution(1001).getData().get().get("id").asInt());
+	}
+
+	@Test
+	public void testResolutions() throws Exception {
+		ResolutionController controller = new ResolutionController(collections);
+		controller.resolutions(2).getData().forEach(x -> assertEquals(2, x.get("council").asInt()));
 	}
 
 }
